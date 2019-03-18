e4bf988337e96581505e5dcd61a580c1fad0d381
Max <miiq@yandex.ru>
2019-03-18 17:13:04 +0300

    Create properties file and suite run files

--- addressbook-web-tests/src/test/java/jqa/maxim/starikov/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/jqa/maxim/starikov/addressbook/tests/TestBase.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite
-  public void setUp() {
+  public void setUp() throws IOException {
     app.init();
   }
 
