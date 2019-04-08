0a5b0db2850ed220e895308ae05088409dde14e6
Max <miiq@yandex.ru>
2019-04-08 16:25:55 +0300

    Added parameter Platform

--- addressbook-web-tests/src/test/java/jqa/maxim/starikov/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/jqa/maxim/starikov/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package jqa.maxim.starikov.addressbook.appmanager;
 
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -48,6 +49,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "linux")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
